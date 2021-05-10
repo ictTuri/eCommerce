67b5852eed7d84abf186c20c2f1e10f80e1cdeca
Artur Molla <mollaartur13@gmail.com>
2021-05-10 23:04:35 +0200

    first commit

---
+++ MisterRobotoArigato/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MisterRobotoArigato.Models.ViewModel
+{
+    /// <summary>
+    /// For the purposes of registering with the store
+    /// </summary>
+    public class RegisterViewModel
+    {
+        public string Email { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Passord")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
